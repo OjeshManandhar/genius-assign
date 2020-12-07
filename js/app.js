90a77665944ac11b90656117c0b9f978b45bde3a
OjeshManandhar <ozes.manandhar@gmail.com>
2020-12-07 18:33:15 +0545

    Complete nav

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 const navInput = document.querySelector('.nav__input');
 const navPlaceholder = document.querySelector('.nav__placeholder');
 
-console.log(navInput, navPlaceholder);
-
 navInput.addEventListener('input', () => {
   if (navInput.value === '') {
+    navInput.classList.remove('bg-white');
     navPlaceholder.classList.remove('d-none');
   } else {
+    navInput.classList.add('bg-white');
     navPlaceholder.classList.add('d-none');
   }
+});
+
+// Upload
+const uploadBtn = document.querySelector('.nav .btn');
+
+uploadBtn.addEventListener('click', () => {
+  console.log('Show Upload Modal');
 });
